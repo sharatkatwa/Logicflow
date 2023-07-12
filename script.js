bbf65c051195be932ed6f4376fcd63cd892379d8
sharatkatwa <sjkatwa2000@gmail.com>
2023-07-12 21:13:27 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+gsap.to('#navbar', {
+  backgroundColor: '#092628',
+  duration: 0.5,
+  height: '90px',
+  scrollTrigger: {
+    trigger: '#navbar',
+    scroller: 'body',
+    // markers: true,
+    start: 'top 0%',
+    end: 'top -10%',
+    scrub: 1,
+  },
+})
+
+gsap.to('#bg-img', {
+  scale: 1.2,
+  duration: 1,
+  scrollTrigger: {
+    trigger: '#bg-img',
+    scroller: 'body',
+    start: 'top 5%',
+    end: 'top -30%',
+    markers: true,
+    scrub: 2,
+  },
+})
